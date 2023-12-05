338cfbf6a5f6bf5bb307a0149f7066a457bb0fc0
Mistayan <helixmastaz@gmail.com>
2023-12-05 13:10:29 +0100

    Update Base.py
    Lint Base

--- src/server/models/Base.py
+++ src/server/models/Base.py
@@ -1,7 +1,14 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 
+"""
+class de Base pour définir les conventions de nommage et autres éléments à appliquer sur tous les objets utilisant cette classe comme 
+Base sqlAlchemy
+"""
 class Base(DeclarativeBase):
+    """
+    définit les conventions de nommage des tables et clées liées aux objets en BDD
+    """
     metadata = MetaData(naming_convention={
         "ix": 'ix_%(column_0_label)s',
         "uq": "uq_%(table_name)s_%(column_0_name)s",
