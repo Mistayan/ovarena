f8ca51af6e9915c0ef1e39bb47291b7226426010
Mistayan <helixmastaz@gmail.com>
2023-12-05 13:31:16 +0100

    Update and rename Player.py to player.py
    Lint player

--- src/server/models/player.py
+++ src/server/models/player.py
@@ -16,27 +16,37 @@ from sqlalchemy import Column, Integer, String, Enum, DateTime
 from . import Base, Direction
 
 class Player(Base):
+    """
+        Base du joueur, à utiliser pour instancier un robot dans l'arène
+    """
     __tablename__ = 'players'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, index=True)
     created_at = Column(DateTime, default=dt.utcnow)
     updated_at = Column(DateTime, default=dt.utcnow, onupdate=dt.utcnow)
     name = Column(String)
-    health = Column(Integer, default=100)
-    inventory = Column(String)
-    x = Column(Integer)
-    y = Column(Integer)
-    direction = Column(Enum(Direction))
     score = Column(Integer, default=0)
 
     def __init__(self, name):
         self.name = name
+        self.score = 0
+
+    def __repr__(self):
+        return f"<Player(name='{self.name}', score='{self.score}')>"
+
+
+class RobotPlayer(Player):
+    """
+        Représentation du joueur dans l'arène (ayant un robot)
+    """
+    health = Column(Integer)
+    inventory = Column(Integer)
+    x = Column(Integer)
+    y = Column(Integer)
+    direction = Column(Enum)
+    
+    def __init__():
         self.health = 100
         self.inventory = []
         self.x = 0
         self.y = 0
         self.direction = Direction.NORTH
-        self.score = 0
-
-    def __repr__(self):
-        return f"<Player(name='{self.name}', health={self.health}," \
-        f"inventory={self.inventory}, x={self.x}, y={self.y}, direction={self.direction})>"
