bd333a260fbc979abb389486497fa66e63f2679d
Mistayan <helixmastaz@gmail.com>
2023-12-17 20:56:23 +0100

    (à finir) état de fin de partie Met la partie en pause et affiche "fin de partie" doit calculer les scores finaux des joueurs, les afficher

---
+++ src/server/states/end_game.py
@@ -0,0 +1,31 @@
+"""
+State class for the state machine
+Handles the game ending operations
+"""
+
+from __future__ import annotations
+
+from src.server.states.base import BaseState
+from src.server.states.possible_states import StateEnum
+
+
+class EndGame(BaseState):
+    """
+    Game just ended
+    Pause game
+    Change arena info to "Game ended, processing results"
+    """
+
+    @property
+    def name(self) -> StateEnum:
+        return StateEnum.END_GAME
+
+    def _on_handle(self):
+        """
+        Pause the game and process the results
+        """
+        self._agent.ruleArena("pause", True)
+        self._agent.ruleArena("info", "🟢 Fin de la partie, traitement des résultats...")
+        self._agent.update()
+
+        ## TODO: process results
