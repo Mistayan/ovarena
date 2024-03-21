79a0589b55d6b35ce856e798f327bc02fab06dbd
Stephen Proust <helixmastaz@gmail.com>
2024-03-21 16:48:24 +0100

    update

--- src/server/rule_changer.py
+++ src/server/rule_changer.py
@@ -16,28 +16,30 @@ agent = Agent(
 )
 
 print(agent.game)
-
-print(agent.game["maxPlayers"])
-rules = {"gridColumns": 3,
-         "gridRows": 4,
-         "map": [
-             [0, 2, 0],
-             [3, 2, 3],
-             [3, 2, 3],
-             [0, 2, 0]],
-
-            "maxPlayers": 5,
-            "maxRobots": 4,
-         }
-
-for key, value in rules.items():
-    agent.ruleArena(key, value)
+agent.ruleArena("pause", False)
 agent.update()
 
-ok = False
-for key, value in agent.game.items():
-    if key == "maxPlayers" and value == 5:
-        ok = True
-    print(key, value)
-
-assert ok
+# print(agent.game["maxPlayers"])
+# rules = {"gridColumns": 3,
+#          "gridRows": 4,
+#          "map": [
+#              [0, 2, 0],
+#              [3, 2, 3],
+#              [3, 2, 3],
+#              [0, 2, 0]],
+#
+#             "maxPlayers": 5,
+#             "maxRobots": 4,
+#          }
+#
+# for key, value in rules.items():
+#     agent.ruleArena(key, value)
+# agent.update()
+#
+# ok = False
+# for key, value in agent.game.items():
+#     if key == "maxPlayers" and value == 5:
+#         ok = True
+#     print(key, value)
+#
+# assert ok
